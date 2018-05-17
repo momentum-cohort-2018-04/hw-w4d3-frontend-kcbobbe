01f2cbdb08d28d0f2e855bf4b960051c5cc73932
kcbobbe <kcbobbe@gmail.com>
2018-05-16 22:36:23 -0400

    got the delete button to work. There are still some problems because the page has to be refreshed for changes to take place when adding or deleting notes.

--- script.js
+++ script.js
@@ -2,25 +2,43 @@ import request from 'superagent'
 
 let NOTES = []
 
-
 function apiUrl (fragment) {
   return `https://notes-api.glitch.me/api/${fragment}`
 }
 
+function onClick(event) {
+  event.preventDefault()
+  console.log('click')
+  const noteId = button.dataset.noteId
+  console.log(noteID)
+}
 
+//  var buttons = document.querySelectorAll('.button-delete')
+//   for(var i = 0; i>buttons.length; i++){
+//     buttons[i].addEventListener('click', function(){ alert("Hello World!")}
+//     )
+//   }
 
+// document.querySelectorAll('.button-delete').forEach(button => {
+//   button.addEventListener('click', event => {
+//     console.log('click')
+//     const noteId = button.dataset.noteId
+//     console.log(noteId);
+//     clickToDelete(noteId)
+//   })})
 
-// function updateNotes (noteId, notes) {
-//   const section = id(sectionId)
-//   const tableBody = section.querySelector('tbody')
-//   tableBody.innerHTML = booksToHTML(books)
-//   tableBody.querySelectorAll('.button-delete').forEach(button => {
-//     button.addEventListener('click', event => {
-//       const bookId = button.dataset.bookId
-//       deleteBook(bookId)
-//     })
-//   })
-// }
+function clickToDelete(noteId) {
+      request
+        .delete(apiUrl(`notes/${noteId}`))
+        .auth('kcbobbe', 'password123')
+        .then(response => {
+        NOTES = NOTES.filter(note => note._id !== noteId)
+        getData()
+
+        })
+    }
+  
+// clickToDelete('J9eb8m6zt9ta2ybA')
 
 document.getElementById('add-note-form').addEventListener('submit', function (event) {
   event.preventDefault()
@@ -47,6 +65,7 @@ document.getElementById('add-note-form').addEventListener('submit', function (ev
     // .catch((err) => {
     //   console.error(err)
     // })
+    getData()
 })
 
 function getData() {
@@ -59,21 +78,58 @@ function getData() {
           `<div class = "note-container">
           <h2>${(result.body.notes[i].title)}</h2>
           <div class = "note-text">${(result.body.notes[i].text)}</div>
-          <button type="button" class="button-delete button-danger" >Delete</button>
+          <button type="button" class="button-delete button-danger"data-note-id="${result.body.notes[i]._id}">Delete</button>
           </div>
           `)
+        
       }
       document.getElementById('posted-notes').innerHTML = NOTES.join("")
-    })
-}
+      document.querySelectorAll('.button-delete').forEach(button => {
+        button.addEventListener('click', event => {
+        const noteId = button.dataset.noteId
+        console.log('click')
+        clickToDelete(noteId)
+      }
 
+    
+)})
+}
+)}
+
+// let buttons = document.querySelectorAll('button')
+//  console.log(buttons)
+// function sayhi(){
+//   console.log('hi')
+// }
+
+
+// document.querySelectorAll('.button-delete').forEach(button => {
+//       button.addEventListener('click', event => {
+//         console.log('click')
+//         const noteId = button.dataset.noteId
+//         console.log(noteId);
+//         // deleteNote(noteId)
+//       })})  
+// buttons[0].addEventListener('click', sayhi())
 getData()
 
-// function clickToDelete(){
-//   container.querySelectorAll('.button-delete').forEach(button => {
-//   button.addEventListener('click', event => {
-//   const noteId = button.dataset.bookId result.body.notes[0]._id
-//   deleteNote('2z7z8WiAdNrMzCv0')
+
+
+
+
+  
+
+// function clickToDelete(noteId) {
+//   note-container.querySelectorAll('.button-delete').forEach(button => {
+//     button.addEventListener('click', event => {
+//       console.log('click')
+//       const noteId = button.dataset.noteId
+//       console.log(noteId);
+//       deleteNote(noteId)
+      
+//   })
+//   })
+// }
 
 
 //   function deleteNote (noteId) {
@@ -85,3 +141,6 @@ getData()
 //         updateAllNotes(NOTES)
 //       })
 //   }
+
+
+    // clickToDelete()
